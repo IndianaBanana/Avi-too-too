3ac13f3c6e225019329dd7b180619b04ff106e4a
IndianaBanana <alkuzhelev@gmail.com>
2025-04-10 19:05:45 +0300

    Initial Commit with spring initializer!

---
+++ Avi-too-too/src/main/java/org/banana/Avi_too_too/AviTooTooApplication.java
@@ -0,0 +1,13 @@
+package org.banana.Avi_too_too;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviTooTooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviTooTooApplication.class, args);
+	}
+
+}
---
+++ Avi-too-too/src/test/java/org/banana/Avi_too_too/AviTooTooApplicationTests.java
@@ -0,0 +1,13 @@
+package org.banana.Avi_too_too;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviTooTooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
